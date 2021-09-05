290d6702f3746fb7c3fac31e5338206a0d299735
alejokope <alejo.kope@despegar.com>
2021-09-05 13:29:22 -0300

    Login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
+  /*{
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
+  },*/
   {
-    path: 'login',
+    path: '',
     loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
   }
 ];
