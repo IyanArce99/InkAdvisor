526ba72ab94c020676d9efc0f0d386e107dac544
alejokope <alejo.kope@despegar.com>
2021-09-08 20:39:08 -0300

    Imgs en cards con transition

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -6,9 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
-
+  pics: string[] =['pic_1.jpg', 'pic_2.jpg', 'pic_1.jpg'];
+  translateXIndex: number = 0;
+  picSelected: number = 0;
   constructor() { }
 
   ngOnInit() {}
+
+  changeImg(position: number): void {
+    this.changeTranslate(position);
+    this.picSelected = position;
+  }
+
+  changeTranslate(newPosition: number): void {
+    let cardContent = document.getElementById('card-content');
+    let width: number = cardContent.offsetWidth;
+
+    this.translateXIndex += (this.picSelected - newPosition) * width;
+  }
+
+
 
 }
