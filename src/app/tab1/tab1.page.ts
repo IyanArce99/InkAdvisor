d22dddefccb393eeb989b7895ec827f8b35fa747
alejokope <alejo.kope@despegar.com>
2021-09-12 21:58:09 -0300

    Agrego maps api

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
-import { Component, NgZone, OnInit } from '@angular/core';
+import { MapsAPILoader } from '@agm/core';
+import { Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FirestoreService } from '../services/firestore.service';
@@ -9,10 +10,31 @@ import { FirestoreService } from '../services/firestore.service';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page implements OnInit {
+  @ViewChild("search") public searchElementRef: ElementRef;
+  private geoCoder;
 
-  constructor(private firestoreService: FirestoreService, private afAuth: AngularFireAuth, private ngZone: NgZone, private router: Router, private activatedRoute: ActivatedRoute) {}
+  constructor(private firestoreService: FirestoreService, private afAuth: AngularFireAuth, private ngZone: NgZone, private router: Router, private activatedRoute: ActivatedRoute, private mapsApi: MapsAPILoader) {}
 
   ngOnInit(): void {
+    this.mapsApi.load().then(() => {
+      this.geoCoder = new google.maps.Geocoder;
+
+      const autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement);
+      autocomplete.addListener("place_changed", () => {
+        this.ngZone.run(() => {
+          //get the place result
+          const place: any = autocomplete.getPlace();
+          //verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+          let locationInfo: any = this.getProvince(place.formatted_address);
+          locationInfo.lat = place.geometry.location.lat();
+          locationInfo.lng = place.geometry.location.lng();
+          console.log(locationInfo)
+        });
+      });
+    });
     this.activatedRoute.params.subscribe(e => {
       this.afAuth.user.subscribe(user => {
         let userData = JSON.parse(localStorage.getItem('userData'));
@@ -33,4 +55,22 @@ export class Tab1Page implements OnInit {
     localStorage.setItem('userData', null);
   }
   
+  getProvince(addres: string) {
+    let arrayAddres: string[] = addres.split(',');
+    let lastElement: number = arrayAddres.length - 1;
+    let provincia: string = arrayAddres[lastElement - 1];
+    let pais: string = arrayAddres[lastElement];
+
+    if (provincia){
+        provincia = provincia.replace(/[\d.-]/g, '').trim();
+    }
+
+    if (pais){
+        pais = pais.replace(/[\d.-]/g, '').trim();
+    }
+    
+    return {
+        pais, provincia
+    };
+}
 }
